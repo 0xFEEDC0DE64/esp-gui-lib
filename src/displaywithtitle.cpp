b4b30c863a44709a99fe6e964b469086692bc67c
0xFEEDC0DE64 <daniel@brunner.ninja>
2023-01-11 14:41:24 +0100

    New design for displays with title (grey bar)

--- src/displaywithtitle.cpp
+++ src/displaywithtitle.cpp
@@ -11,14 +11,17 @@ void DisplayWithTitle::initScreen(TftInterface &tft)
     Base::initScreen(tft);
 
     m_titleLabel.start(tft);
-    tft.fillRect(0, 33, tft.width(), 3, TFT_WHITE);
+
+    tft.fillRect(0, 0, tft.width(), 35, TFT_GREY);
+
+//    tft.fillRect(0, 34, tft.width(), 3, TFT_WHITE);
 }
 
 void DisplayWithTitle::redraw(TftInterface &tft)
 {
     Base::redraw(tft);
 
-    m_titleLabel.redraw(tft, title(), TFT_YELLOW, TFT_BLACK, 4);
+    m_titleLabel.redraw(tft, title(), TFT_WHITE, TFT_GREY, 4);
 }
 
 } // namespace espgui
